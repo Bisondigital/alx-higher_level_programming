a1b7ba55c0040fc01533e049b897f14b8e4e9b36
Bisondigital <jeremiahntuk50@gmail.com>
2022-09-23 12:13:00 -0700

    lit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, 
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul,div
     import sys
 
     if len(sys.argv) - 1 != 3:
